f33ed5ba5244f6fe698373ea096fd71e044ccca2
Marcin Sztolcman <marcin@urzenia.net>
2011-05-18 19:01:28 +0200

    change name from URI_PREFIX to ABSOLUTE_URI (resolve #3)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting (E_ALL|E_STRICT);
 ini_set ('display_errors', 1);
 session_start ();
 
-define ('URI_PREFIX', 'http://filmy.urzenia.net');
+define ('ABSOLUTE_URI', 'http://filmy.urzenia.net');
 
 function debug_array () {
     echo '<pre>';
@@ -84,7 +84,7 @@ if (isset ($_GET['action'])) {
                 debug_array ($e);
                 exit;
             }
-            header ('Location: '.URI_PREFIX);
+            header ('Location: '.ABSOLUTE_URI);
             exit;
         break;
 
@@ -92,7 +92,7 @@ if (isset ($_GET['action'])) {
             if (isset ($_GET['flm_id']) && is_numeric ($_GET['flm_id']) && $_GET['flm_id'] > 0) {
                 $query = $GLOBALS['sql']->prepare ('DELETE FROM `films` WHERE `flm_id` = ?');
                 $query->execute (array ($_GET['flm_id']));
-                header ('Location: '.URI_PREFIX);
+                header ('Location: '.ABSOLUTE_URI);
                 exit;
             }
             else {
@@ -178,7 +178,7 @@ if (isset ($_GET['action'])) {
                     exit;
                 }
 
-                header ('Location: '.URI_PREFIX);
+                header ('Location: '.ABSOLUTE_URI);
                 exit;
             }
             else {
@@ -220,7 +220,7 @@ if (isset ($_GET['action'])) {
                     ));
                 }
 
-                header ('Location: '.URI_PREFIX);
+                header ('Location: '.ABSOLUTE_URI);
                 exit;
             }
             else {
