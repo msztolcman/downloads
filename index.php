27fdd0c96a378a8e21b910a3027572810035c179
Marcin Sztolcman <marcin@urzenia.net>
2011-05-18 19:32:53 +0200

    replace debug_array with cgi_dump - fixes #2 worked 0.1 hours on #2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start ();
 
 define ('ABSOLUTE_URI', 'http://filmy.urzenia.net');
 
-function debug_array () {
+function cgi_dump () {
     echo '<pre>';
     foreach (func_get_args () as $arg) {
         print_r ($arg);
@@ -81,7 +81,7 @@ if (isset ($_GET['action'])) {
                     `flm_dateadd` TEXT NOT NULL DEFAULT ""
                 )');
             } catch (PDOException $e) {
-                debug_array ($e);
+                cgi_dump ($e);
                 exit;
             }
             header ('Location: '.ABSOLUTE_URI);
@@ -174,7 +174,7 @@ if (isset ($_GET['action'])) {
                     }
                 }
                 catch (PDOException $e) {
-                    debug_array ($e);
+                    cgi_dump ($e);
                     exit;
                 }
 
@@ -440,7 +440,7 @@ try {
         );
     }
 } catch (PDOException $e) {
-    debug_array ($e);
+    cgi_dump ($e);
 }
 
 ?>
